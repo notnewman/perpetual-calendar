319cc88e9f70b562bc84e00452ccf0801a7bc65e
NotNewman <notnewman@users.noreply.github.com>
2014-12-31 22:54:13 -0500

    Update index.php
    Added a little jquery trickery to make the page make more sense when crawled.

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html><head>
 <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type"><title>Perpetual Calendar for the years 1753 - 3000</title>
-<meta content="NosyMotorist" name="author">
+<meta content="NosyMotorist" name="author"><script src = "../jquery/jquery.min.js"></script><! Point this at any jquery instance><script type='text/javascript'>$(document).ready(function(){unhidegrid()});</script>
+
 </head>
 <body ONLOAD="onload="init()" style="color: rgb(0, 0, 0); background-color: rgb(204, 204, 204);" alink="#000099" link="#000099" vlink="#990099">
 <?php
@@ -47,7 +48,7 @@ if ($month != 1){
 //
 if ($thefirst == 0) {$thefirst=7;}
 	}
-?>
+?><!document.bigbox.yearblank.focus()>
 <a href="../index.php"><img style="border: 0px solid ; width: 1341px; height: 212px;" alt="Life on the road, commuting to work" title="Life on the road, commuting to work" src="../images/header.jpg"></a>
 <table style="text-align: left; background-color: rgb(204, 204, 204); width: 1313px; height: 1988px;" border="0" cellpadding="1" cellspacing="0">
 <tbody>
@@ -55,6 +56,7 @@ if ($thefirst == 0) {$thefirst=7;}
 <td style="width: 114px; vertical-align: top;"><br>
 </td>
 <td style="width: 579px; vertical-align: top; align: center; font-size: 14pt; font-family: Arial;"><br><table><tr><td style="width: 25%;"></td><td>
+<! onchange=\"chgmonth()\">
 <?php 
 	echo "<form action=\"./\" name=\"bigbox\" id=\"bigbox\" method=\"post\"><select id=\"monthblank\" onchange=\"chgmonth()\">\n";
 	$counter = 1;
@@ -66,9 +68,9 @@ while ($counter < 13){
 			}
 	$counter +=1;
 }
-	echo"</select>&nbsp;&nbsp;&nbsp;&nbsp;<INPUT TYPE=\"text\" NAME=\"yearblank\" style=\"font-size: 10pt; font-family: Arial; color: red;\" SIZE=\"4\" maxlength=\"4\" value=\"".$year."\" onkeyup=\"pop(this)\" onchange=\"chgyear()\" autocomplete=\"off\">&nbsp;&nbsp;&nbsp;&nbsp;<input type=\"button\" VALUE=\"Today\" onclick=\"backtotoday()\"/><br><small><br></small><table><tr>";
+	echo"</select>&nbsp;&nbsp;&nbsp;&nbsp;<INPUT TYPE=\"text\" NAME=\"yearblank\" style=\"font-size: 10pt; font-family: Arial; color: red;\" SIZE=\"4\" maxlength=\"4\" value=\"".$year."\" onkeyup=\"pop(this)\" onchange=\"chgyear()\" autocomplete=\"off\">&nbsp;&nbsp;&nbsp;&nbsp;<input type=\"button\" VALUE=\"Today\" onclick=\"backtotoday()\"/><br><small><br></small><table id = \"cbox\" style = \"display: none;\"><tr>";
 	$counter = 2-$thefirst;
-	$lastday = $alldays[$month];
+	$lastday = $alldays[intval($month)];
 //
 //   Puting day names across the top of the calendar
 	$column =1;
@@ -100,7 +102,7 @@ while ($row <= 6){
 	}
 	echo"</table>";
 ?>
-<span id='fb'></span></form><script type="text/javascript" src="./pcalendar.js"></script>
-<br><br></td></tr></table>Select any month in a year between 1753 and 3000<br><br><small>Why 1753?&nbsp;&nbsp;This is the first year of the calendar we are familiar with; the previous year was shortened by 11 days to compensate for drift between the actual orbital year and that as calculated.</small></td><td style="width: 605px; background-color: silver; vertical-align: top;"><br>
+<span id='fb'></span><script type="text/javascript" src="./pcalendar.js">init();</script></form>
+<br><br></td></tr></table><table><tr><td style="width: 10%;"></td><td>Select any month in a year between 1753 and 3000<br><br><small>Why 1753?&nbsp;&nbsp;This is the first year of the calendar we are familiar with; the previous year was shortened by 11 days to compensate for drift between the actual orbital year and that as calculated.</small><td style="width: 10%;"></td></td></tr></table></td><td style="width: 605px; background-color: silver; vertical-align: top;"><br>
 </td></tr></tbody></table><br>
 </body></html>
