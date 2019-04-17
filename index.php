7c015f448b81ad05f1f549757ed95c1c99a69a42
notnewman <michael@pres-techs.com>
2019-04-17 18:04:15 -0400

    Professional Development commit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html><head>
-<meta content="text/html; charset=ISO-8859-1" http-equiv="content-type"><title>Perpetual Calendar for the years 1753 - 3000</title>
+<title>Perpetual Calendar for the years 1753 - 3000</title>
 <meta content="NosyMotorist" name="author"><script src = "../jquery/jquery.min.js"></script><! Point this at any jquery instance><script type='text/javascript'>$(document).ready(function(){unhidegrid()});</script>
 
 
